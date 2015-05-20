55d3801020f119bacf0a6ab99aa9e866cb43e1bd
Andrew Tomlinson <andrewtomlinson@Andrews-MacBook-Air.local>
2015-05-20 11:19:28 -0400

    directivePractice  update to comments

--- directivePractice/public/app.js
+++ directivePractice/public/app.js
@@ -1,7 +1,8 @@
-// 
+// create a module called 'store' 
 var app = angular.module('store', [])
   
-  .controller('StoreController', function($scope) {
+// create a controller called 'StoreController' that contains an alert welcoming the user to angular
+  .controller('StoreController', function() {
     alert('welcome to angular directives!')
   });
 
